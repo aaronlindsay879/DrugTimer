f9a8ffa9bc86de018f34a023d3a342201b1b9ca7
Aaron <aarlin5409@hessleacademy.com>
2020-09-24 08:56:19 +0100

    trimmed down project structure, added basic framework

---
+++ DrugTimer/Server/Controllers/DrugInformationController.cs
@@ -0,0 +1,32 @@
+﻿using DrugTimer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DrugTimer.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DrugInformationController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public DrugInformationController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DrugInformation> Get()
+        {
+            yield return new DrugInformation
+            {
+                Name = "Codeine",
+                TimeBetweenDoses = 4
+            };
+        }
+    }
+}
---
+++ DrugTimer/Shared/DrugInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrugTimer.Shared
+{
+    public class DrugInformation
+    {
+        public string Name { get; set; }
+        public decimal TimeBetweenDoses { get; set; }
+    }
+}
