dfd73b444e674ff51676da0d20a6322af2da8fcc
Aaron <aarlin5409@hessleacademy.com>
2020-09-27 17:30:38 +0100

    fixed drug display component

---
+++ DrugTimer/Client/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrugTimer.Client.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<T> TakeReversed<T>(this IEnumerable<T> iEnumerable, int num)
+        {
+            var newEnumerable = iEnumerable.Reverse();
+
+            return newEnumerable.Take(num);
+        }
+    }
+}
