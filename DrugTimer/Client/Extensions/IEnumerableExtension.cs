8057d6481f3e5bd1f63f085436803c1b48deaec7
Aaron <aarlin5409@hessleacademy.com>
2020-09-27 19:51:10 +0100

    commented client

--- DrugTimer/Client/Extensions/IEnumerableExtension.cs
+++ DrugTimer/Client/Extensions/IEnumerableExtension.cs
@@ -7,6 +7,13 @@ namespace DrugTimer.Client.Extensions
 {
     public static class IEnumerableExtension
     {
+        /// <summary>
+        /// Takes a number of elements from a list, after reversing it
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="iEnumerable">IEnumerable to use</param>
+        /// <param name="num">Number of elements to take</param>
+        /// <returns>Elements from the list</returns>
         public static IEnumerable<T> TakeReversed<T>(this IEnumerable<T> iEnumerable, int num)
         {
             var newEnumerable = iEnumerable.Reverse();
