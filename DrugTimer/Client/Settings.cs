46dd91977787bc5aa98f1a1c657686db4f500c84
Aaron <aarlin5409@hessleacademy.com>
2020-09-28 20:11:56 +0100

    added persistent storage and cleaned up settings code

---
+++ DrugTimer/Client/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrugTimer.Client
+{
+    public class Settings
+    {
+        public string RefreshRate { get; set; }
+        public string DateFormat { get; set; }
+    }
+}
