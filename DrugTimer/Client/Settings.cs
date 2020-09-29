87bbdbacdc116c04bf8bc737062a71093f149709
Aaron <aarlin5409@hessleacademy.com>
2020-09-29 15:55:04 +0100

    changed default options

--- DrugTimer/Client/Settings.cs
+++ DrugTimer/Client/Settings.cs
@@ -9,5 +9,17 @@ namespace DrugTimer.Client
     {
         public string RefreshRate { get; set; }
         public string DateFormat { get; set; }
+
+        public static Settings Default
+        {
+            get
+            {
+                return new Settings()
+                {
+                    RefreshRate = "5",
+                    DateFormat = "HH:mm dd/MM/yy"
+                };
+            }
+        }
     }
 }
