d8a54a5a25069c29af17ccb6daf78d7e4b85e2f2
Aaron <aarlin5409@hessleacademy.com>
2020-10-01 22:05:40 +0100

    added option for dosages

---
+++ DrugTimer/Client/Data/DrugDosage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrugTimer.Client.Data
+{
+    public class DrugDosage
+    {
+        public string Name;
+        public string Dosage;
+
+        public DrugDosage(string name, string dosage)
+        {
+            Name = name;
+            Dosage = dosage;
+        }
+    }
+}
