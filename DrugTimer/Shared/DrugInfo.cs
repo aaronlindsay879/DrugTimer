d18c1b41261a372ecfca698e66addbf4b1510d5d
Aaron <aarlin5409@hessleacademy.com>
2020-10-01 12:22:17 +0100

    added first implementation of stats

--- DrugTimer/Shared/DrugInfo.cs
+++ DrugTimer/Shared/DrugInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DrugTimer.Shared
@@ -14,5 +15,21 @@ namespace DrugTimer.Shared
         public string Info { get; set; }
 
         public List<DateTime> Entries { get; set; } = new List<DateTime>();
+
+        public decimal Average;
+        public decimal AveragePerDay()
+        {
+            var groupedEntries = Entries.GroupBy(x => (int)TimeSpan.FromTicks(x.Ticks).TotalDays);
+
+            if (groupedEntries.Count() > 0)
+                return groupedEntries.Sum(x => x.Count()) / (decimal)groupedEntries.Count();
+
+            return 0;
+        }
+
+        public void ReCalculateStats()
+        {
+            Average = AveragePerDay();
+        }
     }
 }
